e2509a010dfa738393a713bceae234303157fb1c
Karthickrajaa-v <vkarthickrajaa@gmail.com>
2015-07-20 12:23:33 +0530

    Initial Commit

---
+++ arr.py
@@ -0,0 +1,9 @@
+"""Sample array program"""
+n=[]
+n1=int(raw_input('Enter how many numbers'))
+for i in range(0,n1):
+	n.append(int(raw_input('Enter the Element')))
+print n[3]
+for i in range(0,n1):
+	print n[i]
+print n 
